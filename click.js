4416fb5a76e77905fa1bf02b4f649d847505f88c
AlexSLG <111134750+AlexSLG@users.noreply.github.com>
2024-10-15 08:20:47 -0500

    Update click.js

--- click.js
+++ click.js
@@ -1,9 +1,1 @@
-// Asegúrate de que SweetAlert2 esté incluido en tu proyecto
-document.querySelector('.btn-comprar').addEventListener('click', function() {
-    Swal.fire({
-        title: '¡Compra realizada!',
-        text: 'Gracias por tu compra. Procesaremos tu pedido.',
-        icon: 'success',
-        confirmButtonText: 'Aceptar'
-    });
-});
+
